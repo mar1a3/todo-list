f58f70073bbd330b65611015a29aa44579cfb2b1
capoweb <popytka.nomer1@bk.ru>
2023-06-19 20:25:46 +0000

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -7,9 +7,4 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import glob
 
-# элементы вэбрайвера
-driver = webdriver.Chrome(ChromeDriverManager().install())
-opts = Options()
-opts.add_experimental_option("detach", True)
 
-def get_video():
