367de08c520c48d444f34de509fb694d8836e490
capoweb <popytka.nomer1@bk.ru>
2023-08-20 13:23:15 +0200

    Update korgi.php

--- korgi.php
+++ korgi.php
@@ -625,6 +625,25 @@
     // }
 
 
+// $arr = [
+	// 	'group1'  => ['user11', 
+	// 		'user12', 'user13', 
+	// 		'user43'], 
+	// 	'group2'  => ['user21', 
+	// 		'user22', 'user23'], 
+	// 	'group3'  => ['user31', 
+	// 		'user32', 'user33'], 
+	// 	'group4'  => ['user41', 
+	// 		'user42', 'user43'], 
+	// 	'group5'  => ['user51', 
+	// 		'user52'], 
+	// ];
+
+    // foreach($arr as $key=>$value){
+    //     foreach($value as $value2 =>$elem){
+    //         echo $key.' '. $elem .'<br>';
+    //     }
+    // }
 
 
 
