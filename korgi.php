351264bd9b47cf3a1c5360a9e52eb55980457e2e
capoweb <popytka.nomer1@bk.ru>
2023-08-23 09:59:50 +0200

    Update korgi.php

--- korgi.php
+++ korgi.php
@@ -655,6 +655,26 @@
     // var_dump($arr);
     // // echo '</pre>';
 
+ // $products = [
+        //     [
+        //         'name'   => 'мясо',
+        //         'price'  => 100,
+        //         'amount' => 5,
+        //     ],
+        //     [
+        //         'name'   => 'овощи',
+        //         'price'  => 200,
+        //         'amount' => 6,
+        //     ],
+        //     [
+        //         'name'   => 'фрукты',
+        //         'price'  => 300,
+        //         'amount' => 7,
+        //     ],
+        // ];
+        // foreach($products as $n){
+        //     echo $n['name'].'<br>';
+        // }
 
 
     //урок 152
