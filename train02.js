2d0ccf1fd296c7a0b0211e7d8af88dd61afeefe4
capoweb <popytka.nomer1@bk.ru>
2023-12-11 13:13:02 +0100

    Update train02.js

--- train02.js
+++ train02.js
@@ -744,3 +744,13 @@ findMaxGoodInteger('32000148888')
 //     console.log(result2)
 // };
 // findSpecialInteger([1,2,2,6,6,6,6,7,10])
+
+     //надо было rows сделать columns в матрице
+// var transpose = function(matrix) {
+// let result  = Array.from({length: matrix[0].length},()=> new Array(matrix.length).fill(0));
+// for(let r=0; r<matrix.length;r++){
+//   for(let c=0; c<matrix[0].length; c++){
+//       result[c][r] = matrix[r][c];
+//   }
+// }
+// return result
