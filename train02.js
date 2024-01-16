6d982efaa07c712d28d055d0cf5aad47191348a6
capoweb <popytka.nomer1@bk.ru>
2024-01-16 23:46:25 +0100

    Update train02.js

--- train02.js
+++ train02.js
@@ -611,7 +611,7 @@ each_cons([1,2,3,4], 3)
 // bubbleSort([2,0,2,1,1,0])
 
 //bubbleSort без нового массива
-function bubbleSort(arr){
+// function bubbleSort(arr){
 //     for(let i=0;i<arr.length;i++){
 //         for(let j=0;j<arr.length;j++){
 //         if(arr[j]>arr[j+1]){
